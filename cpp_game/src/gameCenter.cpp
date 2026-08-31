6e48a9f0ba523c1dfdbe659857ad17424e1f45d0
elvis-lyc <elvis96722@gmail.com>
2026-08-31 12:55:04 +0800

    修改舊寫法使其支援 myTool::Point

--- cpp_game/src/gameCenter.cpp
+++ cpp_game/src/gameCenter.cpp
@@ -5,24 +5,25 @@
 namespace gameCenter {
 	namespace {
 		void initGame() {
-			myTool::clearCmd(0, 0, 0, 0, true);
+			myTool::clearCmd({ 0,0 }, { 0,0 }, true);
 			myTool::setCmdTitle(L"Game Application 3.0");
 			myTool::setCursorState(false);
 			myTool::startCursor();
 			myTool::startKeyInput();
 		}
 		void layout() {
-			myTool::myCout("歡迎使用Game Application 3.0，請以滑鼠點擊所需功能", 0, 0, 7);
-			myTool::myCout("貪吃蛇遊戲", 0, 2);
-			myTool::myCout("結束程序", 0, 4);
+			myTool::myCout("歡迎使用Game Application 3.0，請以滑鼠點擊所需功能", { 0,0 }, 7);
+			myTool::myCout("貪吃蛇遊戲", { 0,2 });
+			myTool::myCout("結束程序", { 0,4 });
 		}
 		void clearLayout() {
-			myTool::clearCmd(0, 0, 49, 4);
+			myTool::clearCmd({ 0,0 }, { 49,4 });
 		}
 		void stopGame() {
 			myTool::endCursor();
 			myTool::endKeyInput();
 			myTool::setCursorState(true);
+			myTool::clearCmd({ 0,0 }, { 0,0 }, true);
 		}
 	}
 	void start() {
@@ -32,22 +33,21 @@ namespace gameCenter {
 			myTool::resetCursor();
 			myTool::mySleep(200);
 			myTool::getCursor();
-			myTool::colorChangeLayout("貪吃蛇遊戲", 0, 2, 9, 0, 2);
-			myTool::colorChangeLayout("結束程序", 0, 4, 7, 0, 2);
+			myTool::colorChangeLayout("貪吃蛇遊戲", { 0,2 }, { 9,0 }, 2);
+			myTool::colorChangeLayout("結束程序", { 0,4 }, { 7,0 }, 2);
 			if (myTool::cursorBox.getState() && myTool::cursorBox.getOneClick() &&
 				myTool::cursorBox.getLeftPressed()) {
-				if (myTool::cursorTouchArea(0, 2, 9, 0)) {
+				if (myTool::cursorTouchArea({ 0,2 }, { 9,0 })) {
 					clearLayout();
 					snake::start();
 					layout();
 				}
-				else if (myTool::cursorTouchArea(0, 4, 7, 0)) {
+				else if (myTool::cursorTouchArea({ 0,4 }, { 7,0 })) {
 					break;
 				}
 			}
 		}
 		myTool::setCursorColor();
 		stopGame();
-		myTool::clearCmd(0, 0, 0, 0, true);
 	}
 }
